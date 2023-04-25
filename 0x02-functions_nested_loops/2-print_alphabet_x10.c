716895178ee048e5394b521314834763a49b9348
Nivvergen <Nivvergen@gmail.com>
2023-04-25 15:39:11 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
+
