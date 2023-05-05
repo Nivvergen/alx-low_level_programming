be5d90bc87d8e67e38662dafab2361f12893421f
Nivvergen <Nivvergen@gmail.com>
2023-05-05 16:25:54 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,10 @@
 char *cap_string(char *s)
 {
 	int count = 0, i;
-	int sep_word[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) != *(s + count) - 32;
+		*(s + count) = *(s + count) - 32;
 	count++;
 	while (*(s + count) != '\0')
 	{
@@ -28,5 +28,3 @@ char *cap_string(char *s)
 	}
 	return (s);
 }
-
-
